ce30f08ec607691b0ccb810bccf62f92da52e6a7
naturewaves <frivasfu@purdue.edu>
2025-01-24 22:23:39 -0500

    delete duplicated documentation for the list_waves function

--- R/list_waves.R
+++ R/list_waves.R
@@ -7,19 +7,10 @@
 #'
 #' @examples
 #' \dontrun{
-#' list_waves(folder)
+#' list_waves()
 #' }
 #' List wave files in a directory
 #'
-#' @param folder Character. The path to the folder where the WAV files are stored.
-#'
-#' @return A list of wave files
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' list_waves(folder)
-#' }
 list_waves <- function(folder = NULL){
 
   print_list <- function(list){
